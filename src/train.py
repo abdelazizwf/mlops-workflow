eea93d8ece2933bb23862f5d4d35802206d64db9
Abdelaziz W. Farahat <abdelaziz.w.f@gmail.com>
2025-11-24 05:48:16 +0200

    Add mlflow tracking

--- src/train.py
+++ src/train.py
@@ -1,5 +1,8 @@
+import mlflow
 import pandas as pd
 from sklearn.svm import SVC
+
+mlflow.set_tracking_uri("http://localhost:8080")
 
 train_data = pd.read_csv("prepared_data/train.csv")
 val_data = pd.read_csv("prepared_data/val.csv")
@@ -10,11 +13,17 @@ y_train = train_data["Survived"]
 X_val = val_data.drop(columns=["Survived"])
 y_val = val_data["Survived"]
 
-model = SVC()
+model = SVC(max_iter=1000)
 
-model.fit(X_train, y_train)
-
-train_accuracy = model.score(X_train, y_train)
-val_accuracy = model.score(X_val, y_val)
+with mlflow.start_run() as run:
+    model.fit(X_train, y_train)
+    
+    train_accuracy = model.score(X_train, y_train)
+    val_accuracy = model.score(X_val, y_val)
+    
+    mlflow.log_metrics({
+        "train_accuracy": train_accuracy,
+        "val_accuracy": val_accuracy,
+    })
 
 print(f"Train Accuracy = {train_accuracy:.3f}\nValidation Accuracy = {val_accuracy:.3f}")
